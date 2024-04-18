24c369affa72ce168d49315686c949cc0e2aa688
ismail alif <43023388+alif33@users.noreply.github.com>
2024-04-18 21:29:49 +0600

    edge fix

--- src/components/Brands.tsx
+++ src/components/Brands.tsx
@@ -4,10 +4,10 @@ import Image from "next/image"
 const Brands: React.FC = () => {
  
     return (
-       <div className="flex items-center py-10">
+       <div className="flex items-center py-6">
             <Image
-                height={181}
-                width={453}
+                height={300}
+                width={580}
                 src="/3Tokens/brands.png"
                 alt="Divider between sections"
             />
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,16 +8,16 @@ const Hero: React.FC = () => {
             <div className="flex justify-between py-9 pl-20">
                 <div className="text-white mt-28">
                     <h4 className="text-lg DM-mono opacity-60">Name Stack for HFT Digital Asset Agents</h4>
-                    <div className="py-5">
+                    <div className="py-6">
                         <h1 className="ClashDisplay text-[55px] leading-[58px]">Agent powered <br /> high frequency trading (HFT)</h1>
                     </div>
-                    <h5 className="fira-mono opacity-60">Powered by 3Token</h5>
+                    <h5 className="text-lg fira-mono opacity-60">Powered by 3Token</h5>
                 </div>
                 <div>
                     <Image
                         height={457}
                         width={462}
-                        src="/3Tokens/hero-circle.svg"
+                        src="/3Tokens/hero-circle.png"
                         alt="hero circle"
                     />
                 </div>
